1522667526a0c65a1ef35dd13831906b61670725
Adrian <ab@buerodespraesidenten.de>
2019-12-18 10:54:58 +0100

    Update theme to work with WPSBC plugin 2.x

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const $ = require('jquery');
+const jQuery = require('jquery');
 const Rellax = require('rellax');
 const Flickity = require('flickity');
 const lazyframe = require('lazyframe');
@@ -136,45 +137,3 @@ $('.read-more__button').on('click', function (e) {
     var container = $(this).parent();
     container.find('.read-more__text').fadeToggle();
 });
-
-// SIMPLE BOOKING CALENDAR PLUGIN
-$('div.sbc-calendar-wrapper').each(function() {
-    var $calendar = $(this);
-    
-    function getCurrentMonth() {
-        return $('.sbc-navigation select[name="sbcMonth"]', $calendar).val();
-    }
-    
-    function getCurrentYear() {
-        return $('.sbc-navigation select[name="sbcYear"]', $calendar).val();
-    }
-    
-    function ajaxCalendarUpdate(operation) {
-        var ajaxUrl = $('form', $calendar).attr('action');
-        var data = {
-            action: 'calendarNavigation',
-            operation: operation,
-            month: getCurrentMonth(),
-            year: getCurrentYear(),
-            id: $('form input[name="sbcId"]', $calendar).val()
-        };
-        $('div.sbc-loader', $calendar).addClass('sbc-loader-visible');
-        $.post(ajaxUrl, data, function(response) {
-            $calendar.find('#sbc-calendar').replaceWith(response);
-            $('.sbc-navigation select', $calendar).bind('change', changeMonthOrYear);
-        });
-    }
-    
-    // Prev/next month
-    $($calendar).on('click','a.sbc-prev-month, a.sbc-next-month', function(event) {
-        event.preventDefault();
-        ajaxCalendarUpdate($(this).is('.sbc-prev-month') ? 'prevMonth' : 'nextMonth');
-    });
-    
-    // Custom month/year
-    function changeMonthOrYear () {
-        ajaxCalendarUpdate('date');
-    }
-    $('.sbc-navigation select', $calendar).bind('change', changeMonthOrYear);
-});
-	
